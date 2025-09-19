c02a183d50ee7c05036e07a000f9ea70cd700af8
Rezelix <rezelixvolixant@gmail.com>
2025-09-19 17:05:59 +0800

    add code to print deadlines using stream

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -12,14 +12,21 @@ public class Main {
         Datamanager dataManager = new Datamanager("./data/data.txt");
         ArrayList<Task> tasksData = dataManager.loadData();
 
-        System.out.println("Printing all data ...");
-        printAllData(tasksData);
-
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        printDeadlinesUsingStreams(tasksData);
 
+        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines (using stream): " + countDeadlinesUsingStream(tasksData));
+
+    }
+    private static int countDeadlinesUsingStream(ArrayList<Task> taskData) {
+        int count = (int) taskData.stream()
+                .filter((Task t) -> t instanceof Deadline)
+                .count();
+
+        return count;
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -33,9 +40,17 @@ public class Main {
     }
 
     public static void printAllData(ArrayList<Task> tasksData) {
+        System.out.println("Printing all tasks using iteration...");
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+
+    public static void printAllDataUsingStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing all data using streams ...");
+        tasksData.stream()
+                .forEach(System.out::println);
+
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
